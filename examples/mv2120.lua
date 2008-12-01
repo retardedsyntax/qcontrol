490ab3c645da7227cecaead09bb955de749281a0
Byron Bradley <byron.bbradley@gmail.com>
2008-12-01 19:46:14 +0000

    Add more config files
    git-svn-id: http://qcontrol.googlecode.com/svn/trunk@15 cba486c8-e04f-0410-a7e3-79bda13a29fb

---
+++ examples/mv2120.lua
@@ -0,0 +1,19 @@
+--[[
+	Configuration file for qcontrol (LUA syntax)
+	Supports the HP Media Vault mv2120.
+--]]
+
+-- Requires CONFIG_KEYBOARD_GPIO enabled in the kernel and
+-- the kernel module gpio_keys to be loaded.
+register("evdev", "/dev/input/by-path/platform-gpio-keys-event-",
+	116, "power_button",
+	408, "restart_button")
+
+function power_button( time )
+	os.execute("poweroff")
+end
+
+function restart_button( time )
+	os.execute("reboot")
+end
+
---
+++ examples/ts409.lua
@@ -0,0 +1,60 @@
+--[[
+	Configuration file for qcontrol (LUA syntax)
+	Supports the QNAP TS-409.
+--]]
+
+register("ts409")
+
+-- Requires CONFIG_KEYBOARD_GPIO enabled in the kernel and
+-- the kernel module gpio_keys to be loaded.
+register("evdev", "/dev/input/by-path/platform-gpio-keys-event-",
+	408, "restart_button",
+	133, "media_button")
+
+function power_button( time )
+	os.execute("poweroff")
+end
+
+function restart_button( time )
+	os.execute("reboot")
+end
+
+function media_button( time )
+	piccmd("usbled", "8hz")
+end
+
+fanfail = 0
+
+function fan_error(  )
+	fanfail = fanfail + 1
+	if fanfail == 3 then
+		print("ts409: fan error")
+		piccmd("statusled", "red2hz")
+		piccmd("buzzer", "long")
+	else
+		if fanfail == 10 then
+			fanfail = 0
+		end
+	end
+end
+
+function fan_normal(  )
+	piccmd("statusled", "greenon")
+	fanfail = 0
+end
+
+function temp( temp )
+	print("ts409 temperature:", temp)
+	if temp > 80 then
+		piccmd("fanspeed", "full")
+	else
+		if temp > 70 then
+			piccmd("fanspeed", "high")
+		end
+	else
+		if temp > 55 then
+			piccmd("fanspeed", "medium")
+		end
+	end
+end
+
